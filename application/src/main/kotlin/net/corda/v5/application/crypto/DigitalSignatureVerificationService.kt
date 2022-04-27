936b58de6dfa26f638b0917cd34abff667db5cc4
Alexey Kadyrov <alexey.kadyrov@r3.com>
2022-04-27 09:51:54 +0100

    wip

--- application/src/main/kotlin/net/corda/v5/application/crypto/DigitalSignatureVerificationService.kt
+++ application/src/main/kotlin/net/corda/v5/application/crypto/DigitalSignatureVerificationService.kt
@@ -2,10 +2,7 @@ package net.corda.v5.application.crypto
 
 import net.corda.v5.application.injection.CordaFlowInjectable
 import net.corda.v5.application.injection.CordaServiceInjectable
-import net.corda.v5.crypto.SignatureScheme
-import java.security.InvalidKeyException
 import java.security.PublicKey
-import java.security.SignatureException
 
 /**
  * The [DigitalSignatureVerificationService] digital signature verification operations.
--- application/src/main/kotlin/net/corda/v5/application/crypto/SigningService.kt
+++ application/src/main/kotlin/net/corda/v5/application/crypto/SigningService.kt
@@ -4,14 +4,10 @@ import net.corda.v5.application.injection.CordaFlowInjectable
 import net.corda.v5.application.injection.CordaServiceInjectable
 import net.corda.v5.base.annotations.DoNotImplement
 import net.corda.v5.base.annotations.Suspendable
-import net.corda.v5.crypto.CompositeKey
 import net.corda.v5.crypto.DigestAlgorithmName
 import net.corda.v5.crypto.DigitalSignature
 import net.corda.v5.crypto.SignatureScheme
-import java.security.KeyPair
-import java.security.PrivateKey
 import java.security.PublicKey
-import java.util.*
 
 /**
  * The [SigningService] is responsible for storing and using private keys to sign things. An implementation of this may, for example,
