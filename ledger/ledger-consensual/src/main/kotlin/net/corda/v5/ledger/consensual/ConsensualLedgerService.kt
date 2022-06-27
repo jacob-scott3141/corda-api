4e4e6ffb8455170c89ff014d03a5a9ac1662632e
Christian Sailer <christian.sailer@r3.com>
2022-06-27 18:30:57 +0100

    Add systemflow calls to interface

--- ledger/ledger-consensual/src/main/kotlin/net/corda/v5/ledger/consensual/ConsensualLedgerService.kt
+++ ledger/ledger-consensual/src/main/kotlin/net/corda/v5/ledger/consensual/ConsensualLedgerService.kt
@@ -1,10 +1,27 @@
 package net.corda.v5.ledger.consensual
 
+import net.corda.v5.application.messaging.FlowSession
 import net.corda.v5.base.annotations.DoNotImplement
 import net.corda.v5.base.annotations.Suspendable
+
+data class ConsensualTestRequest(val session: FlowSession)
+
+interface ConsensualTestResponse {
+    val session: FlowSession
+
+    @Suspendable
+    abstract fun createResponse(): String
+}
 
 @DoNotImplement
 interface ConsensualLedgerService {
     @Suspendable
     fun double(n: Int): Int
+
+    @Suspendable
+    fun startTestFlow(request: ConsensualTestRequest): String
+
+    @Suspendable
+    fun receiveTestFlow(request: ConsensualTestResponse)
+
 }
