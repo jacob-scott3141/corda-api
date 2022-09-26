296f896df195774e889c045b94ec70e90ed96c19
Dickon Reed <dickon.reed@r3.com>
2022-09-26 15:57:06 +0100

    CORE-6619: remove BC references

---
+++ application/src/main/kotlin/net/corda/v5/application/crypto/CompositeKeyGenerator.kt
@@ -0,0 +1,10 @@
+package net.corda.v5.application.crypto
+
+import java.security.PublicKey
+
+interface CompositeKeyGenerator {
+
+    /* Return a composite key from a weighted list of keys, and an overall threshold (which can be null, in
+    which case the threshold is the sum of the key weights) */
+    fun create(keys: List<CompositeKeyNodeAndWeight>, threshold: Int?): PublicKey
+}
