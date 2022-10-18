6155523b3e97d0a83280db0de250eb69c746241d
LWogan <lorcan.wogan@r3.com>
2022-10-18 16:49:57 +0100

    CORE-6336 mdc logging fields

---
+++ base/src/main/kotlin/net/corda/v5/base/mdc/FlowMDCFields.kt
@@ -0,0 +1,7 @@
+package net.corda.v5.base.mdc
+
+object FlowMDCFields {
+    const val MDC_CLIENT_ID = "client_Id"
+    const val MDC_VNODE_ID = "vnode_id"
+    const val MDC_EXTERNAL_EVENT_ID = "external_event_id"
+}
