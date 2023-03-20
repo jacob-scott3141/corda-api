98a787a9b4609e98382328acb1cfd797c383350b
nkovacsx <nandor.kovacs@r3.com>
2023-03-20 08:31:20 +0000

    CORE-10383 Custom query execution

--- ledger/ledger-utxo/src/main/java/net/corda/v5/ledger/utxo/UtxoLedgerService.java
+++ ledger/ledger-utxo/src/main/java/net/corda/v5/ledger/utxo/UtxoLedgerService.java
@@ -4,6 +4,7 @@ import net.corda.v5.application.messaging.FlowSession;
 import net.corda.v5.base.annotations.DoNotImplement;
 import net.corda.v5.base.annotations.Suspendable;
 import net.corda.v5.crypto.SecureHash;
+import net.corda.v5.ledger.utxo.query.VaultNamedParameterizedQuery;
 import net.corda.v5.ledger.utxo.transaction.UtxoLedgerTransaction;
 import net.corda.v5.ledger.utxo.transaction.UtxoSignedTransaction;
 import net.corda.v5.ledger.utxo.transaction.UtxoTransactionBuilder;
@@ -125,4 +126,9 @@ public interface UtxoLedgerService {
             @NotNull FlowSession session,
             @NotNull UtxoTransactionValidator validator
     );
+
+    /**
+     * TODO KDocs
+     */
+    @NotNull <R> VaultNamedParameterizedQuery<R> query(@NotNull String queryName, @NotNull Class<R> resultClass);
 }
